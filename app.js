a097949959def8903fdcc02323658cfa619167d0
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-26 02:01:56 -0300

    capturo valores

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const productName = document.querySelector('#productName');
 const productPrice = document.querySelector('#productPrice');
 const buttonSave = document.querySelector('#button-save');
 const buttonCancel = document.querySelector('#button-cancel');
+
+buttonSave.addEventListener('click', () => {
+  const name = productName.value;
+  const price = productPrice.value;
+  console.log(name, price);
+
+})
