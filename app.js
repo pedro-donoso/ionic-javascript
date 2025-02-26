9f4c0425137b9bc9a3f8d95983bf930a46ba48ae
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-26 01:43:31 -0300

    agrego elementos

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log('Works!');
+const productName = document.querySelector('#productName');
+const productPrice = document.querySelector('#productPrice');
+const buttonSave = document.querySelector('#button-save');
+const buttonCancel = document.querySelector('#button-cancel');
